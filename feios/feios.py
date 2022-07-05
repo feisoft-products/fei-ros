3d4c8e1e1ca5802897234dc0f14badfcea01e911
devoter-fyc <fycsfls_winter@126.com>
2022-07-05 20:29:52 +0800

    Added logo.

--- feios/feios.py
+++ feios/feios.py
@@ -1,9 +1,16 @@
 import sys,os,time
 import feios_utils.stdapi as stdapi
+import feios_utils.msg as msg
 
 ################################
 
 stdapi.clear()
+if os.name == 'nt':
+    os.system("color 5f")
+    stdapi.clear()
+    print(msg.STD_BOOT)
+else:
+    print(msg.STD_BOOT)
 print("Welcome to FEI-ROS.")
 print(f"Working on {os.name.upper()}")
 print("Wait for key services to setup...")
@@ -14,6 +21,7 @@ print("We have to ensure that is you.")
 if stdapi._login():
     print("We shall continue...")
     time.sleep(1)
+    stdapi.clear()
 else:
     print("Uh,that password is wrong.Try again.")
     exit(time.sleep(2))
--- feios/feios_utils/stdapi.py
+++ feios/feios_utils/stdapi.py
@@ -10,7 +10,7 @@ import pathlib
 import hashlib
 # Constants.
 version = (0,6,0)
-versuffix = "a3"
+versuffix = "b1"
 __null__ = None
 indev_name = "0.6.0-pre"
 
